a0798f5d0da7b876b9bba9a4038aa26df6777d59
ordazhoyos2001@gmail.com <ordazhoyos2001@gmail.com>
2025-10-08 20:31:29 -0500

    xx

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    //uri: 'https://high-theme-472622-n1.uc.r.appspot.com',
-    uri: 'http://localhost:4000',
+    uri: 'https://high-theme-472622-n1.uc.r.appspot.com',
+    //uri: 'http://localhost:4000',
 }
